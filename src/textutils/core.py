b9f6330ea19ef093f859c8bc3dc85f9d698ca93c
Jad Zoghaib <jadzoghaib@hotmail.com>
2025-10-29 00:55:52 +0100

    Remove docstring from word_count function
    Removed docstring from word_count function.

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,14 +1,4 @@
 def word_count(text):
-	"""Return a dictionary with the count of each word in `text`.
-
-	Words are lower-cased and split on whitespace.
-
-	Example:
-		>>> word_count('Hello hello world')
-		{'hello': 2, 'world': 1}
-	"""
-	from collections import Counter
-
-	words = text.lower().split()
-	return dict(Counter(words))
-
+    from collections import Counter
+    words = text.lower().split()
+    return dict(Counter(words))
