ae6804363c90d8d0c29b9110f29a988dcb41e511
marinmiguel <miguel.marin@alumni.esade.edu>
2025-11-03 15:59:02 +0100

    Update core.py

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,9 +1,4 @@
 def test_normalise_whitespace_remove_extra_spaces():
     text = ' a b \n c '
     assert c.normalise_whitespace(text) == 'a b c'
-def collapse_duplicates(text, char):
-    import re
-    return re.sub(f'{re.escape(char)}+', char, text)
-def is_palindrome(text):
-    cleaned = text.lower().replace(" ", "")
-    return cleaned == cleaned[::-1]
+
