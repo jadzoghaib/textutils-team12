07a7c61681769608a0c145ee343a38f6c8a58f02
Jan Philipp Gnau <janphilipp.gnau@alumni.esade.edu>
2025-11-05 18:15:32 +0100

    emptying test

--- tests/unit/test_core.py
+++
@@ -1,9 +0,0 @@
-def slugify(text):
-    import re
-    import unicodedata
-    text = unicodedata.normalize('NFD', text)
-    text = ''.join(c for c in text if unicodedata.category(c) != 'Mn')
-    text = text.lower()
-    text = re.sub(r'&', 'and', text)
-    text = re.sub(r'[^a-z0-9]+', '-', text)
-    return text.strip('-')
